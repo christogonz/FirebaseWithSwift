1b2737c4f489234c0b9b8d1cbe7a21d0fd9a3e5e
Chris Gonzalez <christogonz92@gmail.com>
2024-08-20 16:35:16 +0200

    Initial Commit

---
+++ FirebaseWithSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseWithSwift
+//
+//  Created by Christopher Gonzalez on 2024-08-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseWithSwift/FirebaseWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseWithSwiftApp.swift
+//  FirebaseWithSwift
+//
+//  Created by Christopher Gonzalez on 2024-08-20.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
